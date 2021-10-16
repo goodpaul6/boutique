b2d6fa4917cc4e1d989a24a8a343275b823248be
Apaar Madan <apaar_madan@hotmail.com>
2021-10-16 18:08:24 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Boutique
+
+A structured key-value store. You define types which will be stored at
+predefined key prefixes and boutique stores and transmits them optimally.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-// Boutique
-// ========
-// A structured key-value store. You define types which will be stored at
-// predefined key prefixes and boutique stores and transmits them optimally.
-
 #include <iostream>
 #include <optional>
 
