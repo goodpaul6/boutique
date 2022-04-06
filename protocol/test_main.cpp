09ba682cadd15a9595d6b5395c9689b2347d33a6
Apaar Madan <apaar_madan@hotmail.com>
2022-04-06 14:07:59 -0400

    Improve protocol test

--- protocol/test_main.cpp
+++ protocol/test_main.cpp
@@ -4,7 +4,10 @@
 #include "binary_protocol.hpp"
 #include "messages.hpp"
 
-int main(int argc, char** argv) {
+namespace {
+
+template <typename R, typename W, typename Fn>
+void write_read_check(W&& value, Fn&& check_fn) {
     using namespace boutique;
 
     std::vector<char> buf;
@@ -14,21 +17,42 @@ int main(int argc, char** argv) {
         return &buf[buf.size() - len];
     };
 
-    GetCommand get_cmd;
-
-    get_cmd.key = "test";
-
-    write(write_fn, get_cmd);
+    write(write_fn, value);
 
     ConstBuffer cbuf{buf.data(), buf.size()};
 
-    Command cmd;
+    R cmd;
 
     auto res = read(cbuf, cmd);
 
     assert(res == ReadResult::SUCCESS);
-    assert(std::holds_alternative<GetCommand>(cmd));
-    assert(std::get<GetCommand>(cmd).key == get_cmd.key);
+    check_fn(cmd);
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+    using namespace boutique;
+
+    GetCommand get_cmd;
+
+    get_cmd.key = "hello";
+
+    SetCommand set_cmd;
+
+    set_cmd.key = "hello";
+    set_cmd.value = "hello";
+
+    write_read_check<Command>(get_cmd, [&](auto& cmd) {
+        assert(std::holds_alternative<GetCommand>(cmd));
+        assert(std::get<GetCommand>(cmd).key == get_cmd.key);
+    });
+
+    write_read_check<Command>(set_cmd, [&](auto& cmd) {
+        assert(std::holds_alternative<SetCommand>(cmd));
+        assert(std::get<SetCommand>(cmd).key == set_cmd.key);
+        assert(std::get<SetCommand>(cmd).value == set_cmd.value);
+    });
 
     return 0;
 }
